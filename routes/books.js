dd621b8e8d5a4f568f28d71cc6c2b62e32390faa
malilasage <malilaclearwater@gmail.com>
2016-11-30 19:27:58 -0700

    part 1 complete

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const express = require('express');
-
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex');
 
 // YOUR CODE HERE
+router.get('/books', function(_req, res, next) {
+  knex('books').orderBy('id').then((books) => {
+    res.send(books);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
 
 module.exports = router;

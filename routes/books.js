e5da4dfa73fc97a4051b93ed17e6126ef56f9751
malilasage <malilaclearwater@gmail.com>
2016-11-30 19:55:39 -0700

    get requests working

--- routes/books.js
+++ routes/books.js
@@ -4,15 +4,36 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const knex = require('../knex');
+const {camelizeKeys, decamelizeKeys} = require('humps');
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
 
 // YOUR CODE HERE
-router.get('/books', function(_req, res, next) {
-  knex('books').orderBy('id').then((books) => {
-    res.send(books);
-  })
-  .catch((err) => {
-    next(err);
-  });
+router.get('/books', function(req, res, next) {
+  knex('books')
+    .orderBy('title')
+    .then((result) => {
+      const book = camelizeKeys(result);
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/books/:id', function(req, res, next) {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if(!book) {
+        return next();
+      }
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;

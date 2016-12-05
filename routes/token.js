f9f709ff417fc93bc59aadc72a8b3cc954570207
malilasage <malilaclearwater@gmail.com>
2016-12-04 18:59:23 -0800

    what the fuk

--- routes/token.js
+++ routes/token.js
@@ -10,28 +10,40 @@ const jwt = require('jsonwebtoken');
 
 // YOUR CODE HERE
 router.get('/token', (req, res, next) => {
-  res.status(200).send(false);
+  const token = req.cookies.token;
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if(err) {
+      return res.send(false);
+    }
+    res.send(true);
+  });
 });
 
 router.post('/token', (req, res, next) => {
-  var reqEmail = req.body.email;
-  var reqPassword = req.body.password;
+  var {reqEmail, reqPassword} = req.body;
+  var token;
   knex('users')
   .where('email', reqEmail)
   .then((result) => {
     var user = result[0];
-    // if(!user) {
-    //   res.send('Bad email or password.');
-    // }
-    // if(bcrypt.compareSync(reqPassword, user.hashed_password) !== true) {
-    //   res.send('Bad email or password!');
-    // }
-    // delete user.hashed_password;
-    // res.setHeader('Content-Type', 'text/plain');
-    // res.send(user);
-    res.cookie({token: true});
+    if(!user) {
+      res.send('Bad email or password.');
+    }
+   if(bcrypt.compareSync(reqPassword, user.hashed_password)) {
+      res.send('Bad email or password!');
+    }
+    else {
+      delete user.hashed_password;
+      var expiry = new Date(Date.now() + 1000 * 60 * 60 * 3);
+      token = jwt.sign({userId: user.id}, process.env.JWT_SECRET, {expiresIn: '3h'})
+    res.cookie('token', token, {
+      httpOnly: true,
+      expires: expiry,
+      // secure: router.get('env') === 'production'
+    });
+  }
     res.send(user);
+    })
   });
-});
 
 module.exports = router;
